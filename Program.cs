2546d50baa2d9c5268f95777a3a043c3caa1d319
nttviet18i1 <48078865+nttviet18i1@users.noreply.github.com>
2019-02-28 11:31:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapthuchanh
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doantau tau1 = new doantau();
+            tau1.nhaptt();
+            tau1.intau();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ doantau.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapthuchanh
+{
+    class doantau
+    {
+        string madoantau;
+        int soluongkhach;
+        public void nhaptt()
+        {
+            Console.WriteLine("nhap ma doan tau");
+            this.madoantau = Console.ReadLine();
+            Console.WriteLine("nhap so luong khach");
+            this.soluongkhach = Convert.ToInt32(Console.ReadLine());
+        }
+        public float tongtien()
+        { float tien; 
+            if (this.soluongkhach < 100)
+       tien =this.soluongkhach*100000;
+        else
+        tien =this.soluongkhach*80000;
+        return tien;
+        }
+        public void intau()
+        {
+            Console.WriteLine("ma doan tau{0} co tien la {1}", this.madoantau, this.tongtien());
+            Console.ReadLine();
+        }
+    }
+}
